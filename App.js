f473d1a2b8980f3a5fb36977aa0f82d3c95c292e
Eduardo <eduardo@launchboom.com>
2023-04-08 19:36:05 -0300

    React Native Color Selection App

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ColorPalette from "./screens/ColorPalette";
+import Home from "./screens/Home";
+import AddColorPalleteModal from "./screens/AddColorPalleteModal";
+
+const RootStack = createStackNavigator();
+const MainStack = createStackNavigator();
+
+const MainStackScreen = () => (
+  <MainStack.Navigator>
+    <MainStack.Screen name="Home" component={Home} />
+    <MainStack.Screen
+      name="ColorPalette"
+      component={ColorPalette}
+      options={({ route }) => ({ title: route.params.title })}
+    />
+  </MainStack.Navigator>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false, presentation: "modal" }}
+        />
+        <RootStack.Screen
+          name="AddNewPalette"
+          component={AddColorPalleteModal}
+          options={{ presentation: "modal" }}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
